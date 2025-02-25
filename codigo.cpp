4af6bac946f4d28afae835b4e71cb0e89cdb9288
MaddoxVG26 <185958@upslp.edu.mx>
2025-02-25 08:26:44 -0600

    Add files via upload

---
+++ codigo.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main ()
+{
+int resp;
+
+printf("inserte 1 o 2");
+scanf("%d", &resp);
+if (resp==1)
+printf("muy bien escogiste 1 ");
+else 
+printf("muy bien escogiste 2 ");
+return 0;
+}
